fcdfd83d628bacc3d4316eedb40645fb752aadc4
Al <71657515+Vinc-Al@users.noreply.github.com>
2026-07-19 12:20:47 +0200

    Add files via upload
    Abjuration

--- config.js
+++ config.js
@@ -223,10 +223,9 @@ const DATA_SHEETS = {
   // sont des clés globales (parent_id + sauvegardes joueur).
   ecoles: {
     necromancie: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFnGDfdGQG1QU1vPTODv-L6YW52rQDIdlO7IMDpr5vty7Q28F44FDsmlmk9m2eY4RDtZs9RPEUcpoC/pub?gid=1918427361&single=true&output=csv",
-    // ↓ Remplace gid=XXXX par le gid de l'onglet Invocation, puis dé-commente :
-    // invocation: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFnGDfdGQG1QU1vPTODv-L6YW52rQDIdlO7IMDpr5vty7Q28F44FDsmlmk9m2eY4RDtZs9RPEUcpoC/pub?gid=XXXX&single=true&output=csv",
     invocation: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFnGDfdGQG1QU1vPTODv-L6YW52rQDIdlO7IMDpr5vty7Q28F44FDsmlmk9m2eY4RDtZs9RPEUcpoC/pub?gid=514887193&single=true&output=csv",
 	transmutation: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFnGDfdGQG1QU1vPTODv-L6YW52rQDIdlO7IMDpr5vty7Q28F44FDsmlmk9m2eY4RDtZs9RPEUcpoC/pub?gid=1977259422&single=true&output=csv",
+  	abjuration: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFnGDfdGQG1QU1vPTODv-L6YW52rQDIdlO7IMDpr5vty7Q28F44FDsmlmk9m2eY4RDtZs9RPEUcpoC/pub?gid=955160217&single=true&output=csv",
   },
 };
 
